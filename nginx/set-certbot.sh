3aae355cf34be6c42216ecb78a25f4b6803090c1
Jerry <fenturechance@gmail.com>
2021-04-14 17:50:47 +0800

    set stdin and tty to ubuntu

--- nginx/set-certbot.sh
+++ nginx/set-certbot.sh
@@ -1,10 +1,17 @@
 echo "============================================================="
+echo "[ install nginx ]"
+echo "============================================================="
+apt-get update
+apt-get -y install nginx
+mv /home/default.conf /etc/nginx/sites-enabled
+echo "============================================================="
 echo "[ set certbot ]"
 echo "============================================================="
-apt-get updatesudo
-apt-get install software-properties-commonsudo add-apt-repository universesudo add-apt-repository ppa:certbot/certbotsudo
+apt-get update
+apt-get -y install software-properties-commonsudo add-apt-repository universesudo add-apt-repository ppa:certbot/certbotsudo
 apt-get update
 certbot --nginx
 echo "============================================================="
 echo "[ finish set certbot ]"
 echo "============================================================="
+service nginx restart
